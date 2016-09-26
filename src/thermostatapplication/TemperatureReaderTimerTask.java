6bbddb71df1ca5d8960920e5d5d037d69e4d5fab
Stefan <stefan.ramilli@gmail.com>
2016-09-26 23:59:21 +0200

    bmp280

--- src/thermostatapplication/TemperatureReaderTimerTask.java
+++ src/thermostatapplication/TemperatureReaderTimerTask.java
@@ -15,6 +15,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import thermostatapplication.devices.BMP280;
 
 /**
  *
@@ -23,15 +26,29 @@ import java.util.TimerTask;
 public class TemperatureReaderTimerTask extends TimerTask {
 
     AdafruitBMP180 tempSensor;
+    BMP280 bmp280;
     float temp;
+    float temp_2;
     Date dateRead;
     String iLocation;
     String iGroup;
     TemperatureStore iTemperatureStore;
+    private static int BUS_1 = 1;
 
     public TemperatureReaderTimerTask(String aLocation, String aGroup, TemperatureStore aTemperatureStore){
         System.out.println("TemperatureReaderTimerTask INSTANTIATED!!!");
         tempSensor = new AdafruitBMP180();
+        try {
+            bmp280 = new BMP280(BMP280.Protocol.I2C, BMP280.ADDR_SDO_2_GND, BUS_1);
+            bmp280.setIndoorNavigationMode();
+            bmp280.setMode(BMP280.Mode.NORMAL, true);
+            bmp280.setTemperatureSampleRate(BMP280.Temperature_Sample_Resolution.TWO, true);
+            bmp280.setPressureSampleRate(BMP280.Pressure_Sample_Resolution.SIXTEEN, true);
+            bmp280.setIIRFilter(BMP280.IIRFilter.SIXTEEN, true);
+            bmp280.setStandbyTime(BMP280.Standby_Time.MS_POINT_5, true);
+        } catch (Exception ex) {
+            Logger.getLogger(TemperatureReaderTimerTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
         temp = 0;
         iLocation = aLocation;
         iGroup = aGroup;
@@ -42,15 +59,22 @@ public class TemperatureReaderTimerTask extends TimerTask {
     public void run(){
         try {
             temp = tempSensor.readTemperature();
+            double[] results = bmp280.sampleDeviceReads();
+            temp_2 = (float) results[BMP280.TEMP_VAL_C];
+            
         } catch (Exception ex) {
             temp = 0;
+            temp_2 = 0;
             ex.printStackTrace();
         }
         dateRead = Helper.resetSecMillsDate(new Date());
         System.out.println("TemperatureReaderTimerTask measure: " +Helper.getDateAsString(dateRead)+" " + Helper.getTempAsString(temp) + " C");
+        System.out.println("TemperatureReaderTimerTask measure_2: " +Helper.getDateAsString(dateRead)+" " + Helper.getTempAsString(temp_2) + " C");
         iTemperatureStore.setLastTemperatureRead(new TemperatureMeasure(iLocation, iGroup, dateRead, temp));
+        //iTemperatureStore.setLastTemperatureRead(new TemperatureMeasure("280", iGroup, dateRead, temp_2));
         if (ThermostatProperties.PERSIST_TEMPERATURES){
             storeTemperature(new TemperatureMeasure(iLocation, iGroup, dateRead, temp));
+            storeTemperature(new TemperatureMeasure("280", iGroup, dateRead, temp_2));
         }
     }
     
